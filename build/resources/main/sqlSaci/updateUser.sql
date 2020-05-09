d88bb3aaaa190dfa0fcf1596807b2b87313b3d9a
ivaneyvieira <ivaneyvieira@gmail.com>
2020-05-09 10:34:11 -0300

    Pronto para testar

--- build/resources/main/sqlSaci/updateUser.sql
+++
@@ -1,8 +0,0 @@
-INSERT IGNORE INTO sqldados.abastecimentoLoc(no, abreviacoes)
-SELECT no, auxStr
-FROM sqldados.users;
-
-UPDATE sqldados.abastecimentoLoc AS L INNER JOIN sqldados.users AS U USING (no)
-SET bits2       = :bitAcesso,
-    abreviacoes = :abreviacoes
-WHERE login = :login
