85df7dcff0a74e91a077918cc1daf5dc534966ac
ivaneyvieira <ivaneyvieira@gmail.com>
2020-05-09 10:34:11 -0300

    Pronto para testar

--- build/libs/exploded/impresssoPedido-1.0.war/WEB-INF/classes/sqlSaci/findAbreviacoes.sql
+++
@@ -1,7 +0,0 @@
-SELECT MID(localizacao, 1, 4) AS abreviacoes
-FROM sqldados.prdloc
-GROUP BY abreviacoes
-HAVING abreviacoes LIKE 'CD%' AND
-       abreviacoes NOT LIKE 'CD00' AND
-       length(abreviacoes) = 4
-ORDER BY abreviacoes
