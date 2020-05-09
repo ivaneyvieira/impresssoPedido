5d0e0470ec7bab9ea263c201e3067f18150eb0cd
ivaneyvieira <ivaneyvieira@gmail.com>
2020-05-09 11:09:34 -0300

    Pronto para testar 2

--- src/main/resources/sqlSaci/listaProdutos.sql
+++ src/main/resources/sqlSaci/listaProdutos.sql
@@ -1,10 +1,10 @@
-SELECT LPAD(prdno * 1, 6, '0')           AS codigo,
-       TRIM(MID(P.name, 1, 37))          AS descricao,
-       E.grade                           AS grade,
-       TRIM(MID(P.name, 37, 3))          AS un,
-       E.qtty / 1000                     AS qtd,
-       E.price / 100                     AS vlUnit,
-       (E.qtty / 1000) * (E.price / 100) AS vlTotal
+SELECT CAST(LPAD(prdno * 1, 6, '0') AS CHAR) AS codigo,
+       TRIM(MID(P.name, 1, 37))              AS descricao,
+       E.grade                               AS grade,
+       TRIM(MID(P.name, 37, 3))              AS un,
+       E.qtty / 1000                         AS qtd,
+       E.price / 100                         AS vlUnit,
+       (E.qtty / 1000) * (E.price / 100)     AS vlTotal
 FROM sqldados.eoprd       AS E
   INNER JOIN sqldados.prd AS P
 	       ON P.no = E.prdno
